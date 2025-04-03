0c87b88d1da4bf9913c5ebfaf6db27668acd45c6
Ethan Blanche <blancheq@bc.edu>
2025-04-03 15:01:22 -0400

    Initial Commit

---
+++ TimeSnap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeSnap
+//
+//  Created by Ethan Blanche on 4/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeSnap/TimeSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeSnapApp.swift
+//  TimeSnap
+//
+//  Created by Ethan Blanche on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TimeSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
